db43566f7ef7b2120d07ffd35f3ef9ad90e1cd1c
Pietro Romano <pietroro98@gmail.com>
2026-03-20 09:37:41 +0100

    first commit

---
+++ src/main/java/it/prova/gestionalepizzeria/GestionalepizzeriaApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestionalepizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionalepizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionalepizzeriaApplication.class, args);
+	}
+
+}
